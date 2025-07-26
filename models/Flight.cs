092b4fc7188bc0ca4d1cd7126a5933d9645ab1c8
amrbadran <badran.amr2003@gmail.com>
2025-07-26 12:57:19 +0300

    Add Flight.cs & FlightClassEnum.cs

---
+++ models/Flight.cs
@@ -0,0 +1,20 @@
+using airport_ticket_booking_system.models.enums;
+
+namespace airport_ticket_booking_system.models;
+
+public class Flight
+{
+    public double Price { get; set; }
+
+    public string DepartureCountry { get; set; }
+
+    public string DestinationCountry { get; set; }
+
+    public DateTime DepartureDate { get; set; }
+
+    public Airport DepartureAirport { get; set; }
+
+    public Airport ArrivalAirport { get; set; }
+
+    public FlightClassEnum FlighClass { get; set; }
+}
