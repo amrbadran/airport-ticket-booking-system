aa21d9a1c31826e1e8ced691f6a3e57c5a6e628f
amrbadran <badran.amr2003@gmail.com>
2025-07-26 14:15:03 +0300

    Add ValidationAttribute Parent

---
+++ validation/attributes/ValidationAttribute.cs
@@ -0,0 +1,8 @@
+namespace airport_ticket_booking_system.validation.attributes;
+
+[AttributeUsage(AttributeTargets.Property,AllowMultiple = true)]
+public abstract class ValidationAttribute
+{
+    public abstract bool isValid(Object obj);
+    public abstract string ErrorMessage();
+}
