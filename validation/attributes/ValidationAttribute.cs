947094420bb57ac13bd2c67ef555f351a6291c94
amrbadran <badran.amr2003@gmail.com>
2025-07-27 23:14:57 +0300

    remove validation folder

--- validation/attributes/ValidationAttribute.cs
+++
@@ -1,8 +0,0 @@
-namespace airport_ticket_booking_system.validation.attributes;
-
-[AttributeUsage(AttributeTargets.Property,AllowMultiple = true)]
-public abstract class ValidationAttribute : Attribute
-{
-    public abstract bool isValid(Object obj);
-    public abstract string ErrorMessage();
-}
